904063be002311955ac7b33c4f04f30fcc4f7e04
Michael Tibbetts <michael.tibbetts.1@gmail.com>
2024-10-28 10:35:54 -0400

    completed and ready for submission

--- exercises.js
+++ exercises.js
@@ -88,3 +88,95 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 //Exercise 7//
 
+//Exercise 8//
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+
+
+// function sentence(noun = 'cat', adjective = 'white') {
+//     return `The ${noun} is ${adjective}.`
+    
+// }
+// console.log(sentence())
+
+
+
+//Exercise 9//
+
+// Convert the following `if...else` statement in to a ternary:
+
+// let pizza = 'tasty';
+
+// // if (pizza === 'tasty') {
+// //   console.log('yum');
+// // } else {
+// //   console.log('yuck');
+// // }
+
+// // Your code here
+// let taste = pizza === 'tasty' ? 'yum' : 'yuck'
+
+// console.log(taste)
+
+
+
+//Exercise 10//
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+// const localLangConfig = 'es';  // Change to 'es', 'fr', etc., or keep it null
+
+// // a. Create a variable called LANG
+// // b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// // Your code here
+// let defaultLang = 'en'
+
+// let LANG = localLangConfig || defaultLang
+
+// // Log the result
+// console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+// const userSavedTheme = 'dark'; // Change to 'dark', 'contrast', etc., or keep it null
+
+// // a. Create a variable called USER_THEME
+// // b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// // Your code here
+
+// let USER_THEME = userSavedTheme || 'light'
+
+// // Log the result
+// console.log('User theme setting:', USER_THEME);
+
+
+
+//Exercise 11//
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+// const adventurer = {
+//     name: 'Alice',
+//   };
+  
+//   let cat = adventurer.cat?.name; // Your code here
+  
+
+
+//   console.log(cat);
+  
+
+
+
+
